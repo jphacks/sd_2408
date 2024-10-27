c9c2897f7d3b218b95e5a56265032bfd3368c42b
seg <segnities007@gmail.com>
2024-10-27 13:27:29 +0900

    create getUser

--- server/http/get.go
+++ server/http/get.go
@@ -2,9 +2,10 @@ package http
 
 import (
 	"net/http"
-	// "server/model"
-
+	"server/model"
+	"server/db"
 	"github.com/labstack/echo/v4"
+
 )
 
 func Gets(e *echo.Echo){
@@ -16,9 +17,21 @@ func Gets(e *echo.Echo){
 	e.GET("/get/hashtag", getHashtag)
 }
 
-func getUser(c echo.Context)error{
-	return c.String(http.StatusOK, "test")
+func getUser(c echo.Context) error {
+	email := c.FormValue("email")
+	password := c.FormValue("password")
+
+	var user model.User
+
+	// DBからユーザーを取得
+	if err := db.GetUser(c, email, password, &user); err != nil {
+		return err // すでにエラーメッセージが含まれている
+	}
+
+	return c.JSON(http.StatusOK, user) // ユーザー情報を返す
 }
+
+
 
 func getGroup(c echo.Context)error{
 	return c.String(http.StatusOK, "test")
